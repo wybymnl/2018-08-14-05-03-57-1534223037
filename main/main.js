35e88ac59e5c1ca1f553c76eb5aa104a273ce36d
wybymnl <wangyanboymnl@163.com>
2018-08-16 15:41:28 +0800

    main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your code here
+  if(str==null)
+	  return -1;
+  let num = parseInt(str);
+  var sum = 0;
+  for(;num>=1;num/=10){
+      var temp = parseInt(num%10);
+      sum += temp;
+  }
+  return sum;
 };
